568690e8a6e7b5cf095b76bfeb45c97f0533c805
Pratik Jain <pratik862pj@live.com>
2017-08-19 02:17:41 +0530

    Added FirstScreen Partially

--- LibraryManagement/MainWindow.xaml.vb
+++ LibraryManagement/MainWindow.xaml.vb
@@ -1,5 +1,17 @@
-﻿Class MainWindow
+﻿Imports System.Windows.Media.Animation
+'Commented things for personal use do not DELETE
+Class MainWindow
+    Dim showWin As Storyboard
+    Dim hideWin As Storyboard
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
-        Me.Background = New SolidColorBrush(Colors.Red)
+        'Me.Background = New SolidColorBrush(Colors.Red)
+        'BeginStoryboard(showWin)
+        'grabs contents of page1
+        Dim page1 As New Page1()
+        Me.Content = page1
+    End Sub
+    Private Sub Me_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        'showWin = Me.Resources("showWin")
+        'hideWin = Me.Resources("hideWin")
     End Sub
 End Class
---
+++ LibraryManagement/logininPage.xaml.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Media.Animation
+
+Class Page1
+    Dim movetxt As Storyboard
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        BeginStoryboard(movetxt)
+        txtWelcome.Text = "Welcome, John Doe"
+        txtLoginInstruction.Text = "Please enter your PIN"
+    End Sub
+    Private Sub Me_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        movetxt = Me.Resources("moveText")
+    End Sub
+End Class
