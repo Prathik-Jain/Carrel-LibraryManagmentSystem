502e1038702fb280db06f64b66ca9829770f020e
Pratik Jain <pratik862pj@live.com>
2017-08-20 02:21:55 +0530

    Added Numberpad For PIN

--- LibraryManagement/logininPage.xaml.vb
+++ LibraryManagement/logininPage.xaml.vb
@@ -1,13 +1,20 @@
 ﻿Imports System.Windows.Media.Animation
 
 Class Page1
+    Dim mw As New MainWindow
     Dim movetxt As Storyboard
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
         BeginStoryboard(movetxt)
         txtWelcome.Text = "Welcome, John Doe"
         txtLoginInstruction.Text = "Please enter your PIN"
     End Sub
+
     Private Sub Me_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         movetxt = Me.Resources("moveText")
+    End Sub
+
+    Private Sub Close_Click_1(sender As Object, e As RoutedEventArgs) Handles Close.Click
+        Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown
+        Application.Current.Shutdown()
     End Sub
 End Class
