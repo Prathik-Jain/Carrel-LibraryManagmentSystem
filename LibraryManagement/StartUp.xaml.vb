90ab5b3cb1fbc987a1617e6e6b6f0691231abfe7
Pratik Jain <pratik862pj@live.com>
2017-09-25 01:45:47 +0530

    The startup window for time being

---
+++ LibraryManagement/StartUp.xaml.vb
@@ -0,0 +1,18 @@
+﻿'Imports System.Windows.Media.Animation
+'Commented things for personal use do not DELETE
+Class StartUp
+    'Dim camera As New Camera
+    'Dim showWin As Storyboard
+    'Dim hideWin As Storyboard
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        'Me.Background = New SolidColorBrush(Colors.Red)
+        'BeginStoryboard(showWin)
+        'grabs contents of page1
+        Dim page1 As New Page1()
+        Me.Content = page1
+    End Sub
+    Private Sub Me_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        'showWin = Me.Resources("showWin")
+        'hideWin = Me.Resources("hideWin")
+    End Sub
+End Class
