fe9a3cb61325eecb44eeb66d2e4bdf69f89446cc
Shreya SU <shrey128@outlook.com>
2017-10-01 17:03:21 +0530

    Removed Tabs and Added PopBox for Form access

--- LibraryManagement/DashBoard.xaml.vb
+++ LibraryManagement/DashBoard.xaml.vb
@@ -1,3 +1,14 @@
 ﻿Public Class DashBoard
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        MemberHost.IsOpen = True
 
+
+
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As RoutedEventArgs)
+        BookHost.IsOpen = True
+
+
+
+    End Sub
 End Class
