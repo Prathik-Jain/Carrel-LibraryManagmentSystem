76d625cdb6849a1a0c43ac739c968c54fedcbc04
Pratik Jain <pratik862pj@live.com>
2017-10-02 03:47:22 +0530

    :arrow_upper_right: UI Improvements :recycle: Code clean up

--- LibraryManagement/DashBoard.xaml.vb
+++ LibraryManagement/DashBoard.xaml.vb
@@ -1,8 +1,10 @@
 ﻿Public Class DashBoard
-    Private Sub BtnMemb_Click(sender As Object, e As RoutedEventArgs)
-        MemberHost.IsOpen = True
+    Private Sub BtnMember_OnClick(sender As Object, e As RoutedEventArgs)
+        MemberForm.IsOpen = True
     End Sub
-    Private Sub BtnBook_Click(sender As Object, e As RoutedEventArgs)
-        BookHost.IsOpen = True
-      End Sub
+
+    Private Sub BtnBook_OnClick(sender As Object, e As RoutedEventArgs)
+        BookForm.IsOpen = True
+    End Sub
+
 End Class
