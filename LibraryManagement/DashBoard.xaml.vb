6bb5d27c6fc7e10eb417d1f70f614fb79c68bf73
Pratik Jain <pratik862pj@live.com>
2017-10-02 20:17:50 +0530

    Added Naming and FAB Functionality

--- LibraryManagement/DashBoard.xaml.vb
+++ LibraryManagement/DashBoard.xaml.vb
@@ -1,10 +1,21 @@
-﻿Public Class DashBoard
-    Private Sub BtnMember_OnClick(sender As Object, e As RoutedEventArgs)
-        MemberForm.IsOpen = True
+﻿Imports MaterialDesignThemes.Wpf
+
+Public Class DashBoard
+    Private Sub BtnAddBook_Click(sender As Object, e As RoutedEventArgs) Handles BtnAddBook.Click
+        BookFormDialog.IsOpen = True
+        FAB.IsEnabled = False
     End Sub
 
-    Private Sub BtnBook_OnClick(sender As Object, e As RoutedEventArgs)
-        BookForm.IsOpen = True
+    Private Sub BtnAddMember_Click(sender As Object, e As RoutedEventArgs) Handles BtnAddMember.Click
+        MemberFormDialog.IsOpen = True
+        FAB.IsEnabled = False
     End Sub
 
+    Private Sub BookFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) Handles BookFormDialog.DialogClosing
+        FAB.IsEnabled=True
+    End Sub
+
+    Private Sub MemberFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) Handles MemberFormDialog.DialogClosing
+        FAB.IsEnabled=True
+    End Sub
 End Class
