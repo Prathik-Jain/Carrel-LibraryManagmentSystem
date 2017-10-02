42cc6fcb2a74b206ee1f432e34969a6b7ddabb80
Pratik Jain <pratik862pj@live.com>
2017-10-03 00:22:11 +0530

    :new: Add Members to DataBase

--- LibraryManagement/MemberForm.xaml.vb
+++ LibraryManagement/MemberForm.xaml.vb
@@ -1,16 +1,21 @@
-﻿Public Class MemberForm
+﻿Imports MaterialDesignThemes.Wpf
+
+Public Class MemberForm
     Private Async Sub BtnAccept_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccept.Click
+
         Dim member as New Member With {
                 .FName = TxtFirstName.Text,
-                .Lname = TxtLastName.Text,
+                .LName = TxtLastName.Text,
                 .Phone = TxtPhone.Text,
                 .Dept = TxtDepartment.Text,
                 .Sem = TxtSemester.Text
                 }
-        If (Await MemberService.AddMember(member))
-            MsgBox("Added")
+        If Await MemberService.AddMember(member) Then
+            DashBoard.SnackBarMessageQueue.Enqueue("Registered "+ TxtFirstName.Text+ " as Member.", "UNDO", Sub()
+                MsgBox("Undo Clicked")
+            End Sub)
         Else
-            MsgBox("Not Added")
+            DashBoard.SnackBarMessageQueue.Enqueue("Failed registering "+ TxtFirstName.Text) 
         End If
     End Sub
 End Class
