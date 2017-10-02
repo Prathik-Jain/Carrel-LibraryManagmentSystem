35e4ff6e1fb38d56a17b3d37099cb645e741ed60
Pratik Jain <pratik862pj@live.com>
2017-10-02 20:18:58 +0530

    Added sub routine to add member once add button is clicked

--- LibraryManagement/MemberForm.xaml.vb
+++ LibraryManagement/MemberForm.xaml.vb
@@ -1,4 +1,16 @@
-﻿Public Class UserControl1
-    Private Sub Button_Click(sender As Object)
+﻿Public Class MemberForm
+    Private Async Sub BtnAccept_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccept.Click
+        Dim member as New Member With {
+                .FName = TxtFirstName.Text,
+                .Lname = TxtLastName.Text,
+                .Phone = TxtPhone.Text,
+                .Dept = TxtDepartment.Text,
+                .Sem = TxtSemester.Text
+                }
+        If (Await MemberService.AddMember(member))
+            MsgBox("Added")
+        Else
+            MsgBox("Not Added")
+        End If
     End Sub
 End Class
