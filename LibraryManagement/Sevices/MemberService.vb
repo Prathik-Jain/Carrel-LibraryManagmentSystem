e012b6b8ba80ffbc03143c07f73fd811b9940952
Pratik Jain <pratik862pj@live.com>
2017-10-02 20:17:03 +0530

    Add AddMember Function

---
+++ LibraryManagement/Sevices/MemberService.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+Imports Carrel
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Serialization
+
+Public Class MemberService
+    Friend Shared Function AddMember(member As Member) As Task(Of Integer)
+        Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = new SqlCommand("INSERT INTO JSON (CATEGORY,DATA) VALUES ('MEM','"+JsonConvert.SerializeObject(member)+"')" ,connection)
+        connection.Open()
+        Return query.ExecuteNonQueryAsync()
+        query.Connection.Close()
+    End Function
+
+End Class
