64b65a6245fd93b6ee38cafab4a8a6dd85722e2d
Pratik Jain <pratik862pj@live.com>
2017-10-03 00:21:53 +0530

    :white_check_mark: Added SnackBar

--- LibraryManagement/DashBoard.xaml.vb
+++ LibraryManagement/DashBoard.xaml.vb
@@ -1,21 +1,28 @@
 ﻿Imports MaterialDesignThemes.Wpf
 
 Public Class DashBoard
+    Public Shared SnackBarMessageQueue as SnackbarMessageQueue
+
     Private Sub BtnAddBook_Click(sender As Object, e As RoutedEventArgs) Handles BtnAddBook.Click
+
         BookFormDialog.IsOpen = True
         FAB.IsEnabled = False
     End Sub
 
     Private Sub BtnAddMember_Click(sender As Object, e As RoutedEventArgs) Handles BtnAddMember.Click
+        SnackBarMessageQueue = Snackbar.MessageQueue
         MemberFormDialog.IsOpen = True
         FAB.IsEnabled = False
     End Sub
 
-    Private Sub BookFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) Handles BookFormDialog.DialogClosing
-        FAB.IsEnabled=True
+    Private Sub BookFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) _
+        Handles BookFormDialog.DialogClosing
+        FAB.IsEnabled = True
     End Sub
 
-    Private Sub MemberFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) Handles MemberFormDialog.DialogClosing
-        FAB.IsEnabled=True
+
+    Private Sub MemberFormDialog_DialogClosing(sender As Object, eventArgs As DialogClosingEventArgs) _
+        Handles MemberFormDialog.DialogClosing
+        FAB.IsEnabled = True
     End Sub
 End Class
