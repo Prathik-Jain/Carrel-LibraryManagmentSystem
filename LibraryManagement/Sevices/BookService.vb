c2f799cbe3478503381c4eb5c50f3d550eb94f44
Pratik Jain <pratik862pj@live.com>
2017-10-08 00:54:36 +0530

    Added Book Service

---
+++ LibraryManagement/Sevices/BookService.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+Imports Newtonsoft.Json
+
+Public Class BookService
+    Friend Shared Function AddBook(Book As Object, number As Integer) As Task(Of Integer)
+        Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = New SqlCommand("INSERT INTO JSON VALUES ('BOOK',@JSONString,@Number)", connection)
+        query.Parameters.Add(New SqlParameter("@JSONString", JsonConvert.DeserializeObject(Book)))
+        query.Parameters.Add(New SqlParameter("@Number", number))
+        connection.Open()
+        Return query.ExecuteNonQueryAsync()
+        query.Connection.Close()
+    End Function
+
+End Class
