f222b8debc359c9259875488cf00bcb9bb3133c2
Pratik Jain <pratik862pj@live.com>
2017-10-08 23:01:54 +0530

    Functionality to add department from Database

--- LibraryManagement/MemberForm.xaml.vb
+++ LibraryManagement/MemberForm.xaml.vb
@@ -1,13 +1,26 @@
-﻿Imports MaterialDesignThemes.Wpf
-Imports Newtonsoft.Json
+﻿Imports Newtonsoft.Json
 Public Class MemberForm
-    Private Async Sub BtnAccept_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccept.Click
-
-        Dim member As Object = New Linq.JObject()
+    Private Sub BtnAccept_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccept.Click
+        AddMember()
+        AddDept()
+    End Sub
+    Private Sub MemberForm_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        CmbDept.ItemsSource = DepatmentService.GetDept()
+    End Sub
+        Private Sub AddDept()
+        For Each item In CmbDept.Items
+            If CmbDept.Text = item
+                Exit Sub
+                End If
+            Next
+        DepatmentService.AddDepatment(CmbDept.Text)
+    End Sub
+    Private Async Sub AddMember()
+         Dim member As Object = New Linq.JObject()
                 member.FName = TxtFirstName.Text
                 member.LName = TxtLastName.Text
                 member.Phone = TxtPhone.Text
-                member.Dept = TxtDepartment.Text
+                member.Dept = CmbDept.Text
                 member.Sem = TxtSemester.Text
         If Await MemberService.AddMember(member) Then
             DashBoard.SnackBarMessageQueue.Enqueue("Registered "+ TxtFirstName.Text+ " as Member.", "UNDO", Sub()
