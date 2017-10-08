d1f7c837f6ceaae8cac87b21af8001ddfcf307db
Pratik Jain <pratik862pj@live.com>
2017-10-08 23:02:21 +0530

    fixed :bug:

--- LibraryManagement/Sevices/MemberService.vb
+++ LibraryManagement/Sevices/MemberService.vb
@@ -4,9 +4,10 @@ Imports Newtonsoft.Json
 Imports Newtonsoft.Json.Serialization
 
 Public Class MemberService
-    Friend Shared Function AddMember(member As Member) As Task(Of Integer)
+    Friend Shared Function AddMember(member As Object) As Task(Of Integer)
         Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
-        Dim query = new SqlCommand("INSERT INTO JSON (CATEGORY,DATA) VALUES ('MEM','"+JsonConvert.SerializeObject(member)+"')" ,connection)
+        Dim query = new SqlCommand("INSERT INTO JSON (CATEGORY,DATA) VALUES ('MEM',@JSONString)" ,connection)
+        query.Parameters.Add(New SqlParameter("@JSONString",JsonConvert.SerializeObject(member)))
         connection.Open()
         Return query.ExecuteNonQueryAsync()
         query.Connection.Close()
