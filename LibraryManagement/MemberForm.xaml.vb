0a46dae7cfdd66c5bb114ca437b2f7ca5e15c9f1
Pratik Jain <pratik862pj@live.com>
2017-10-08 20:51:03 +0530

    :arrow_upper_right: Added dynamic member object

--- LibraryManagement/MemberForm.xaml.vb
+++ LibraryManagement/MemberForm.xaml.vb
@@ -1,15 +1,14 @@
 ﻿Imports MaterialDesignThemes.Wpf
-
+Imports Newtonsoft.Json
 Public Class MemberForm
     Private Async Sub BtnAccept_Click(sender As Object, e As RoutedEventArgs) Handles BtnAccept.Click
 
-        Dim member as New Member With {
-                .FName = TxtFirstName.Text,
-                .LName = TxtLastName.Text,
-                .Phone = TxtPhone.Text,
-                .Dept = TxtDepartment.Text,
-                .Sem = TxtSemester.Text
-                }
+        Dim member As Object = New Linq.JObject()
+                member.FName = TxtFirstName.Text
+                member.LName = TxtLastName.Text
+                member.Phone = TxtPhone.Text
+                member.Dept = TxtDepartment.Text
+                member.Sem = TxtSemester.Text
         If Await MemberService.AddMember(member) Then
             DashBoard.SnackBarMessageQueue.Enqueue("Registered "+ TxtFirstName.Text+ " as Member.", "UNDO", Sub()
                 MsgBox("Undo Clicked")
