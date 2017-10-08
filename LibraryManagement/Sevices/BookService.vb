85590a68f09dcfa5797fd11b551e02b0668769aa
Pratik Jain <pratik862pj@live.com>
2017-10-08 16:49:47 +0530

    :bug: SerializedJson.

--- LibraryManagement/Sevices/BookService.vb
+++ LibraryManagement/Sevices/BookService.vb
@@ -5,7 +5,7 @@ Public Class BookService
     Friend Shared Function AddBook(Book As Object, number As Integer) As Task(Of Integer)
         Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
         Dim query = New SqlCommand("INSERT INTO JSON VALUES ('BOOK',@JSONString,@Number)", connection)
-        query.Parameters.Add(New SqlParameter("@JSONString", JsonConvert.DeserializeObject(Book)))
+        query.Parameters.Add(New SqlParameter("@JSONString", JsonConvert.SerializeObject(Book)))
         query.Parameters.Add(New SqlParameter("@Number", number))
         connection.Open()
         Return query.ExecuteNonQueryAsync()
