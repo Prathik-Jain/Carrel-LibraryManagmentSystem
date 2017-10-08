0d8362cc81cc350f4eb66626db5a829060359d49
Shreya SU <shrey128@outlook.com>
2017-10-08 13:40:00 +0530

    Card design

--- LibraryManagement/Card.xaml.vb
+++ LibraryManagement/Card.xaml.vb
@@ -1,33 +1,35 @@
 ﻿Imports Newtonsoft.Json
-Imports System.Windows.Interop
 Imports System.Drawing
 Public Class Card
     Dim admin As Admin
-    Public sub UpdateCard()
-         Dim x As  DashBoard
+    Public Sub UpdateCard()
+        Dim x As DashBoard
         x = Application.Current.Windows(0)
         Dim data = New ArrayList
-         data = MemberService.PrintLastAdded()
-        LblName.Content = Data(0) + " " + Data(1)
-        LblPhone.Content = Data(2)
+        data = MemberService.PrintLastAdded()
+        LblName.Content = data(0) + " " + data(1)
+        LblPhone.Content = data(2)
         LblDepartment.Content = data(3)
-        LblUID.Content = Data(4)
+        LblUID.Content = data(4)
         Dim product As Object = New Linq.JObject
         product.UID = LblUID.Content
         product.Name = LblName.Content
         Dim QRJson = JsonConvert.SerializeObject(product)
         Dim qr = New ZXing.BarcodeWriter()
         qr.Format = ZXing.BarcodeFormat.QR_CODE
+        qr.Options = New ZXing.Common.EncodingOptions
+        qr.Options.Width = 150
+        qr.Options.Height = 150
         Dim result = New Bitmap(qr.Write(QRJson))
-        ImgQR.Source =ImageSourceForBitmap(result)
-        
-        x.a.card.Content= Me
+        ImgQR.Source = ImageSourceForBitmap(result)
+
+        x.a.card.Content = Me
         x.PrintQRDialog.IsOpen = True
 
-    End sub
-        public Function ImageSourceForBitmap(bmp As Bitmap)
-    
+    End Sub
+    Public Function ImageSourceForBitmap(bmp As Bitmap)
+
         Dim handle = bmp.GetHbitmap()
-            return Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromWidthAndHeight(200,200))
-        End Function
+        Return Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions())
+    End Function
 End Class
---
+++ LibraryManagement/ViewBook.xaml.vb
@@ -0,0 +1,3 @@
+﻿Public Class ViewBook
+
+End Class
