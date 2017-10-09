2b4459f519c674e29aa68a33839533b381bff760
Shreya SU <shrey128@outlook.com>
2017-10-10 01:20:41 +0530

    :x: Deleted Card Holder.Xaml

--- LibraryManagement/CardHolder.xaml.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class CardHolder
-    Private Sub BtnPrint_Click(sender As Object, e As RoutedEventArgs) Handles BtnPrint.Click
-        card.Measure(new Size(206,365))
-        card.Arrange(new Rect(new Size(816, 1056)))
-        card.UpdateLayout
-        Dim printDlg As New PrintDialog
-        printDlg.PrintVisual(card,"Card" )
-    End Sub
-End Class
