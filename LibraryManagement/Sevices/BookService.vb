57b1b9d6829946ffce46eb11a4efbca1ea2e94d1
Pratik Jain <pratik862pj@live.com>
2017-10-09 14:39:30 +0530

    Added PrintLastAdded Function

--- LibraryManagement/Sevices/BookService.vb
+++ LibraryManagement/Sevices/BookService.vb
@@ -12,4 +12,23 @@ Public Class BookService
         query.Connection.Close()
     End Function
 
+        Friend Shared Function PrintLastAdded()
+         Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = new SqlCommand("SELECT * FROM BOOK WHERE ID = (SELECT MAX(ID) FROM BOOK)" ,connection)
+        connection.Open()
+        Dim reader As SqlDataReader
+        reader = query.ExecuteReader
+        Dim data As New ArrayList
+        reader.Read
+
+            data.add(reader("ISBN").ToString)
+            data.Add(reader("TITLE").ToString)
+            data.Add(reader("AUTHOR").ToString)
+            data.Add(reader("PUBLISHER").ToString)
+            data.Add(reader("EDITION").ToString)
+            data.Add(reader("PRICE").ToString)
+            data.Add(reader("RACK").ToString)
+      Return data
+    
+    End Function
 End Class
