6e7ca9ceac5a8d6e05433ce06d5322b196ec0630
Pratik Jain <pratik862pj@live.com>
2017-10-09 23:48:36 +0530

    Added Try-Finally Block

--- LibraryManagement/Sevices/MemberService.vb
+++ LibraryManagement/Sevices/MemberService.vb
@@ -1,49 +1,59 @@
 ﻿Imports System.Data.SqlClient
-Imports Carrel
 Imports Newtonsoft.Json
-Imports Newtonsoft.Json.Serialization
 
 Public Class MemberService
+
     Friend Shared Function AddMember(member As Object) As Task(Of Integer)
-        Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
-        Dim query = new SqlCommand("INSERT INTO JSON (CATEGORY,DATA) VALUES ('MEM',@JSONString)" ,connection)
-        query.Parameters.Add(New SqlParameter("@JSONString",JsonConvert.SerializeObject(member)))
-        connection.Open()
-        Return query.ExecuteNonQueryAsync()
-        query.Connection.Close()
+        Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = New SqlCommand("INSERT INTO JSON (CATEGORY,DATA) VALUES ('MEM',@JSONString)", connection)
+        query.Parameters.Add(New SqlParameter("@JSONString", JsonConvert.SerializeObject(member)))
+        Try
+            connection.Open()
+            Return query.ExecuteNonQueryAsync()
+        Finally
+            query.Connection.Close()
+        End Try
     End Function
 
     Friend Shared Function PrintLastAdded()
-         Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
-        Dim query = new SqlCommand("SELECT * FROM MEMBER WHERE ID = (SELECT MAX(ID) FROM MEMBER)" ,connection)
-        connection.Open()
-        Dim reader As SqlDataReader
-        reader = query.ExecuteReader
-        Dim data As New ArrayList
-        reader.Read
+        Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = New SqlCommand("SELECT * FROM MEMBER WHERE ID = (SELECT MAX(ID) FROM MEMBER)", connection)
+        Try
+            connection.Open()
+            Dim reader As SqlDataReader
+            reader = query.ExecuteReader
+            Dim data As New ArrayList
+            reader.Read()
 
-            data.add(reader("FNAME").ToString)
+            data.Add(reader("FNAME").ToString)
             data.Add(reader("LNAME").ToString)
             data.Add(reader("PHONE").ToString)
             data.Add(reader("DEPT").ToString)
             data.Add(reader("SEM").ToString)
             data.Add(reader("UID").ToString)
-      Return data
+            Return data
+        Finally
+            query.Connection.Close()
+        End Try
+
     End Function
 
     Friend Shared Function EditMember(UID As String, member As Object) As Task(Of Integer)
-                Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
-        Dim query = new SqlCommand("UPDATE Member 
+        Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = New SqlCommand("UPDATE Member 
                                     SET FNAME = @FName, LNAME = @LName, PHONE = @Phone, DEPT = @Dept, SEM = @Sem
-                                    WHERE UID = @UID" ,connection)
-        query.Parameters.Add(New SqlParameter("@FName",member("FName").ToString))
+                                    WHERE UID = @UID", connection)
+        query.Parameters.Add(New SqlParameter("@FName", member("FName").ToString))
         query.Parameters.Add(New SqlParameter("@LName", member("LName").ToString))
         query.Parameters.Add(New SqlParameter("@Phone", member("Phone").ToString))
         query.Parameters.Add(New SqlParameter("@Dept", member("Dept").ToString))
-        query.Parameters.Add(New SqlParameter("@Sem",member("Sem").ToString))
+        query.Parameters.Add(New SqlParameter("@Sem", member("Sem").ToString))
         query.Parameters.Add(New SqlParameter("@UID", UID))
-        connection.Open()
-        Return query.ExecuteNonQueryAsync()
-        query.Connection.Close()
+        Try
+            connection.Open()
+            Return query.ExecuteNonQueryAsync()
+        Finally
+            query.Connection.Close()
+        End Try
     End Function
 End Class
