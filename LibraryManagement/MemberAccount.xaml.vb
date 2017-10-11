8735fb922a67e288df49679c8453d00db0c39b06
Pratik Jain <pratik862pj@live.com>
2017-10-11 20:29:21 +0530

    Added sub procedure to get data from database

--- LibraryManagement/MemberAccount.xaml.vb
+++ LibraryManagement/MemberAccount.xaml.vb
@@ -1,3 +1,29 @@
-﻿Public Class MemberDetails
+﻿Imports Newtonsoft.Json
 
+Public Class MemberAccount
+    Dim dashBoard As DashBoard = Application.Current.Windows(0)
+    Dim admin As New Admin
+    Public Sub GetData(UID As String)
+        Dim data As New ArrayList
+        data = MemberService.GetMember(UID)
+        LblUID.Content = UID
+        LblName.Content = data(0) + "" + data(1)
+        LblPhone.Content = data(2)
+        LblDepartment.Content = data(3)
+        LblSemester.Content = data(4)
+        Dim Borrowed = JsonConvert.DeserializeObject(data(6))
+        Dim BookList As New List(Of Book)
+        For Each item As Linq.JObject In Borrowed
+            BookList.Add(New Book With {.BookID = item.GetValue("UID"), .BorrowedOn = item.GetValue("On"), .Title = item.GetValue("Title")})
+        Next
+        BorrowedList.ItemsSource = BookList
+        dashBoard.MemberPopup.Content = Me
+        dashBoard.MemberPopupDialog.IsOpen = True
+    End Sub
+
+End Class
+Class Book
+    Property BookID
+    Property Title
+    Property BorrowedOn
 End Class
