7621bfdd5f4077ab184fa18158b2baf352fe7c39
Pratik Jain <pratik862pj@live.com>
2017-10-12 23:09:23 +0530

    :heavy_plus_sign: GetBooks() and AddBorrowe()

--- LibraryManagement/Sevices/BookService.vb
+++ LibraryManagement/Sevices/BookService.vb
@@ -12,7 +12,28 @@ Public Class BookService
         query.Connection.Close()
     End Function
 
-        Friend Shared Function PrintLastAdded()
+    Friend Shared Function GetBooks(MemberID As String) As List(Of Book)
+        Dim list As New List(Of Book)
+        Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = new SqlCommand("SELECT UID, TITLE, BORROWEDON FROM BOOK WHERE BORROWEDBY = @MemberID" ,connection)
+        Dim reader As SqlDataReader
+        Dim data As New ArrayList
+        Try
+            query.Parameters.Add(New SqlParameter("@MemberID", MemberID))
+            connection.Open
+            reader= query.ExecuteReader
+            Dim count =0
+            While reader.Read
+                list.Add(New Book With {.Sl = count, .BookID = reader("UID").ToString, .BorrowedOn = reader("BORROWEDON").ToString, .Title = reader("TITLE").ToString})
+                count += 1
+            End While
+            Return list
+        Finally
+            query.Connection.Close
+        End Try
+    End Function
+
+    Friend Shared Function PrintLastAdded()
          Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
         Dim query = new SqlCommand("SELECT * FROM BOOK WHERE ID = (SELECT MAX(ID) FROM BOOK)" ,connection)
         connection.Open()
@@ -30,5 +51,23 @@ Public Class BookService
             data.Add(reader("RACK").ToString)
       Return data
     
+    End Function
+
+    Friend Shared Function AddBorrower(BookID  As String,MemberID As String) As Integer
+         Dim connection = new SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = new SqlCommand("UPDATE Book
+                                    SET BORROWEDBY = @MemberID, BORROWEDON = @NOW
+                                    WHERE UID = @BooKID" ,connection)
+        
+        Try
+            query.Parameters.Add(New SqlParameter("@MemberID",MemberID))
+            query.Parameters.Add(New SqlParameter("@BooKID",BookId))
+            query.Parameters.Add(New SqlParameter("@NOW",Now.Date()))
+            connection.Open
+            Return query.ExecuteNonQuery
+            
+            Finally
+            query.Connection.Close
+        End Try
     End Function
 End Class
