2038b1c3498e2a4bd93d45dc715e88301fd4f515
Pratik Jain <pratik862pj@live.com>
2017-10-15 01:39:42 +0530

    Minor Changes

--- LibraryManagement/MemberAccount.xaml.vb
+++ LibraryManagement/MemberAccount.xaml.vb
@@ -5,25 +5,23 @@ Public Class MemberAccount
     Dim _dashBoard As DashBoard = Application.Current.Windows(0)
     WithEvents _sendImage As DispatcherTimer
     Dim _camera As Camera
-    Public sub New
+    Public sub New ()
 
         ' This call is required by the designer.
         InitializeComponent()
-
         ' Add any initialization after the InitializeComponent() call.
     End Sub
-    Public Async Sub GetData(uid As String)
-        Dim data As New ArrayList
-        data = MemberService.GetMember(UID)
+    Public Async sub GetData(uid As String)
+        dim data as ArrayList = await MemberService.GetMember(UID)
         BorrowedList.ItemsSource = new List(Of Book) (Await BookService.GetBooksBorrowed(UID))
         LblUID.Content = UID
         LblName.Content = data(0) + "" + data(1)
         LblPhone.Content = data(2)
         LblDepartment.Content = data(3)
         LblSemester.Content = data(4)
-        _dashBoard.MemberPopup.Content = Me
+        _dashBoard.MemberPopup.Content = me
         _dashBoard.MemberPopupDialog.IsOpen = True
-    End Sub
+    End sub
     Public Sub StartCameraAndTimer()
         _camera = New Camera
         _camera.StartCamera()
@@ -33,10 +31,10 @@ Public Class MemberAccount
         _sendImage.Start()
     End Sub
 
-    Public Sub StopCameraAndTimer()
-        _camera.StopCamera()
+    Public sub StopCameraAndTimer() 
+         _camera.StopCamera()
         _sendImage.Stop()
-    End Sub
+    End sub
 
     Public Sub SendImage_Tick(sender As Object, e As EventArgs) Handles _sendImage.Tick
         Dim qrDecoder As New QRDecoder
@@ -57,7 +55,7 @@ Public Class MemberAccount
         Try
             If jsonString.Contains("UID") Then
                 if Await CheckBookInList 'Checks and returns book if found
-                    updateList("MEM-0001")
+                     updateList("MEM-0001")
                 ElseIf await BookService.Borrowed("BOOK-2","MEM-0001") 
                     updateList("MEM-0001")
                 Else 
@@ -75,7 +73,6 @@ Public Class MemberAccount
 
     Private async Sub UpdateList(memberId As String)
         BorrowedList.ItemsSource =new List(Of Book) (await BookService.GetBooksBorrowed("MEM-0001"))
-        Await Task.Delay(50)
         StartCameraAndTimer
     End Sub
 
@@ -92,8 +89,8 @@ Public Class MemberAccount
 
     Private Sub MemberAccount_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
         StartCameraAndTimer
-
     End Sub
+
 End Class
 Class Book
     Property Sl
