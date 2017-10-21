28fd58b6950eed193116cf0fbb318ad8be32a7f1
Pratik Jain <pratik862pj@live.com>
2017-10-21 17:23:06 +0530

    Minor changes

--- LibraryManagement/ViewBook.xaml.vb
+++ LibraryManagement/ViewBook.xaml.vb
@@ -5,16 +5,16 @@ Public Class ViewBook
         ' This call is required by the designer.
         InitializeComponent()
     End Sub
-    Dim _x As DashBoard
+    Dim _dashboard As DashBoard
     Dim _data = New ArrayList
 
      Friend Sub ViewLastRecord(number As Integer)
-        _x = Application.Current.Windows(0)
+        _dashboard = Application.Current.Windows(0)
         _data = BookService.GetLastAdded()
         UpdateView(_data)
         LblNoofBooks.Content = number
-        _x.BookView.Content = Me
-        _x.ViewBookDialog.IsOpen = True
+        _dashboard.BookView.Content = Me
+        _dashboard.ViewBookDialog.IsOpen = True
     End Sub
     Friend Sub ViewBookById(bookId As String)
         _data = bookservice.GetBookByID(bookID)
@@ -33,8 +33,8 @@ Public Class ViewBook
             'LblBorrowedBy.Content = MemberService.GetMemberByID(data(8)) + "[" + data(8) + "]"
             'LblBorrrowedOn.Content = data(9)
         End If
-        _x.BookView.Content = me
-        _x.ViewBookDialog.IsOpen =True
+        _dashboard.BookView.Content = me
+        _dashboard.ViewBookDialog.IsOpen =True
     End Sub
 
     Sub UpdateView(data)
@@ -45,5 +45,16 @@ Public Class ViewBook
         LblEdition.Content = data(4)
         LblPrice.Content = data(5)
         LblRack.Content = data(6)
+    End Sub
+    Private Sub BtnEdit_Click(sender As Object, e As RoutedEventArgs) Handles BtnEdit.Click
+        _dashBoard.ViewBookDialog.IsOpen = False
+        _dashBoard.BookForm.TxtISBN.Text = LblISBN.Content
+        _dashBoard.BookForm.TxtTitle.Text = LblTitle.Content
+        _dashBoard.BookForm.LstAuthor.ItemsSource = LblAuthors.Content
+        _dashBoard.BookForm.TxtPublisher.Text = LblPublisher.Content
+        _dashBoard.BookForm.CmbEdition.Text = LblEdition.Content
+        '_dashBoard.BookForm.LblUID.Content = LblUID.Content
+        _dashBoard.BookForm.BtnAccept.Content = "UPDATE"
+        _dashBoard.MemberFormDialog.IsOpen = True
     End Sub
 End Class
