9f1ff3a7a1e39cd8d48c31e16282aad86b91c2a8
Pratik Jain <pratik862pj@live.com>
2017-10-22 04:08:08 +0530

    :heavy_plus_sign: Add and edit Admin

--- LibraryManagement/AdminForm.xaml.vb
+++ LibraryManagement/AdminForm.xaml.vb
@@ -4,25 +4,59 @@ Public Class AdminForm
       ' This call is required by the designer.
         InitializeComponent()
     End Sub
-    Private Sub FieldLostFocus(sender As Object, e As RoutedEventArgs)
-        Dim txt = DirectCast(sender, TextBox)
-        txt.GetBindingExpression(TextBox.TextProperty).UpdateSource()
-    End Sub
+    'Private Sub FieldLostFocus(sender As Object, e As RoutedEventArgs)
+    '    Dim txt = DirectCast(sender, TextBox)
+    '    txt.GetBindingExpression(TextBox.TextProperty).UpdateSource()
+    'End Sub
 
     Private Sub TxtPhone_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtPhone.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^0-9]")
     End Sub
 
-    Private Sub TxtFirstName_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtFirstName.PreviewTextInput
+    Private Sub TxtFirstName_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtFName.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^a-zA-Z]+$")
     End Sub
 
-    Private Sub TxtLastName_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtLastName.PreviewTextInput
+    Private Sub TxtLastName_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtLName.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^a-zA-Z]+$")
     End Sub
 
-    Private Sub TxtPassword_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtPassword.PreviewTextInput
+    Private Sub TxtPassword_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtPin.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^0-9]")
     End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As RoutedEventArgs) Handles BtnAdd.Click
+        AddAdmin()
+    End Sub
+    private async sub AddAdmin()
+        dim admin as Object = New Newtonsoft.Json.Linq.JObject
+        admin.FName = TxtFName.Text
+        admin.LName = TxtLName.Text
+        admin.Phone = TxtPhone.Text
+        admin.Pin = TxtPin.Text
+        admin.SecurityQuestion = CmbSecurityQ.Text
+        admin.Answer = TxtAnswer.Text
+        if lblUid.content = ""
+            if await AdminService.AddAdmin(admin) then
+                AdminPopup.SnackBarMessageQueue.Enqueue(TxtFName.Text + " Registered as Admin", "PRINT" , sub()
+                                                                                                              dim adminpopup as New AdminPopup
+AdminPopup.Print(admin)
+                                                                                              End Sub)
+            else
+                AdminPopup.SnackBarMessageQueue.Enqueue("Failed Registering")
+            End If
+        else
+            if await AdminService.EditAdmin(admin, lbluid.content) then
+                AdminPopup.SnackBarMessageQueue.Enqueue(" Updated Admin Details", "PRINT" , sub()
+                                                                                                 dim adminpopup as New adminpopup
+AdminPopup.Print(admin)
+                End Sub)
+            else
+                AdminPopup.SnackBarMessageQueue.Enqueue("Failed Updating Details")
+            End If
+        End If
+
+    End sub
+
 
 End Class
