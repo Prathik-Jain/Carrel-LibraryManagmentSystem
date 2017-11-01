fbec1d1bcd3a0285844360c27eff679f72bf18a1
Pratik Jain <pratik862pj@live.com>
2017-11-01 23:42:39 +0530

    Initial Connection - for no lag! :tada:

--- LibraryManagement/StartUp.xaml.vb
+++ LibraryManagement/StartUp.xaml.vb
@@ -1,9 +1,27 @@
-﻿Class StartUp
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
 
+Class StartUp
+    Public Sub New
+        InitializeComponent()
+    End Sub
+    Private Async Function InitialConnection() As Task(Of Boolean)
+        Dim connection = New SqlConnection(ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
+        Dim query = New SqlCommand("SELECT 'STARTUP'", connection)
+        Try
+            connection.Open()
+            Return Await query.ExecuteNonQueryAsync()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw
+        Finally
+            query.Connection.Close()
+        End Try
+    End Function
     Private Async Sub Me_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
-        Await Task.Delay(8000)
-        Dim login as new Login
+        Await InitialConnection
+        Dim login As New Login
         login.Show()
-        me.Close()
+        Me.Close()
     End Sub
 End Class
