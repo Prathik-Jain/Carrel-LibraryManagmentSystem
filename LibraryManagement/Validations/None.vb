a84a587d535112f6a20ad6a48323689734fa677e
Pratik Jain <pratik862pj@live.com>
2017-11-05 17:35:36 +0530

    :arrow_upper_right: Documentation comments.

--- LibraryManagement/Validations/None.vb
+++ LibraryManagement/Validations/None.vb
@@ -1,7 +1,14 @@
 ﻿Imports System.Globalization
 Namespace Validation
+    ''' <summary>
+    ''' </summary>
 Public Class None
     Inherits ValidationRule
+        ''' <summary>
+        ''' This function is used to check if the textfield is empty
+        ''' </summary>
+        ''' <param name="value">Value in the textfield.</param>
+        ''' <returns> As this function is called when the content of textfield is  changed; it would return true as the field is not empty.</returns>
     Public Overrides Function Validate(value As Object, cultureInfo As CultureInfo) As ValidationResult
            Return New ValidationResult(True,"")
     End Function
