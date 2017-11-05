801f4483dfd540c9d9ec22df98df39c92427ec3b
Pratik Jain <pratik862pj@live.com>
2017-11-05 17:31:42 +0530

    :arrow_upper_right: Documentation Comments

--- LibraryManagement/Validations/ISBNLength.vb
+++ LibraryManagement/Validations/ISBNLength.vb
@@ -1,13 +1,22 @@
 ﻿Imports System.Globalization
 Namespace Validation
+''' <summary>
+''' This class is used to check the length of the ISBN number - i.e., 10.
+''' </summary>
 Public Class IsbnLength
-    		Inherits ValidationRule
+			Inherits ValidationRule
+		''' <summary>
+		''' This function validates if the length of the entered ISBN is 10
+		''' <para>The same function is used to check the size of phone number in <see cref="MemberForm"/></para>
+		''' </summary>
+		''' <param name="value">The value in the textfiled.</param>
+		''' <returns>A validationResult either True or False with a message.</returns>
 		Public Overrides Function Validate(value As Object, cultureInfo As CultureInfo) As ValidationResult
-        If value.ToString.Trim.Length = 10
-                        Return New ValidationResult(True,"")
-              Else 
-            Return New ValidationResult(False,"Must be a 10 digit number")
-        End If              
+		If value.ToString.Trim.Length = 10
+						Return New ValidationResult(True,"")
+			  Else 
+			Return New ValidationResult(False,"Must be a 10 digit number")
+		End If              
 		End Function
 End Class
-    End Namespace
+	End Namespace
