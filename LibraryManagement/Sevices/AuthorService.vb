e9a310f398e626f0da961916787e406fb86af15b
Pratik Jain <pratik862pj@live.com>
2017-11-05 15:47:20 +0530

    :arrow_upper_right: Documentation comments.

--- LibraryManagement/Sevices/AuthorService.vb
+++ LibraryManagement/Sevices/AuthorService.vb
@@ -1,6 +1,12 @@
 ﻿Imports System.Data.SqlClient
-
+''' <summary>
+''' Contains all the functions related to an Author.
+''' </summary>
 Public Class AuthorService
+    ''' <summary>
+    ''' Gets all the authors from the <c>Author</c> table.
+    ''' </summary>
+    ''' <returns>All the list of authors from the Database.</returns>
     Public Shared Function GetAuthors() As ArrayList
         Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
         Dim dr as SqlDataReader
@@ -20,9 +26,13 @@ Public Class AuthorService
         Finally
             query.connection.Close()
         End Try
-       
     End Function
-
+''' <summary>
+''' This function is used to add new Authors to the Database if not already present in the database. 
+''' <para>Grabs the name from Add Book form.</para>
+''' </summary>
+''' <param name="author">Name of the author to be added.</param>
+''' <seealso cref="Carrel.BookForm"/>
     Friend Shared Async Sub AddAuthor(author As String)
    Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
         Dim query = New SqlCommand("INSERT INTO AUTHOR VALUES (@Author)" ,connection)
