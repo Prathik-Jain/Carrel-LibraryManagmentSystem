44f102439eebfc244a516339212034fb0372cacc
Pratik Jain <pratik862pj@live.com>
2017-11-05 16:28:27 +0530

    :arrow_upper_right: Documentation comments.

--- LibraryManagement/Sevices/BookService.vb
+++ LibraryManagement/Sevices/BookService.vb
@@ -1,8 +1,17 @@
 ﻿Imports System.Data
 Imports System.Data.SqlClient
 Imports Newtonsoft.Json
+''' <summary>
+''' Handles all the functions related to books in the library.
+''' </summary>
 
 Public Class BookService
+    ''' <summary>
+    ''' This function adds a new book to the <c>JSON</c> table which calls a trigger to add it to the <c>Book</c> table.
+    ''' </summary>
+    ''' <param name="book">Dynamic object of book which contains all the details about the book.</param>
+    ''' <param name="number">Total number of books to be added.</param>
+    ''' <returns></returns>
     Friend Shared Async Function AddBook(book As Object, number As Integer) As Task(Of Integer)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -19,7 +28,11 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' Gets the list of books borrowed by a particular member.
+    ''' </summary>
+    ''' <param name="memberId">UID of a member.</param>
+    ''' <returns>The a table of books with Unique ID, Title, and the date on which it was borrowed.</returns>
     Friend Shared function GetBooksBorrowed(memberId As String) 
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -39,7 +52,10 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End function
-
+    ''' <summary>
+    ''' Gets the details of the book which was last added.
+    ''' </summary>
+    ''' <returns>An arrary list of books with all the basic fields.</returns>
     Friend Shared Function GetLastAdded() As ArrayList
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -66,7 +82,12 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' This function is responsible to handle the books returned by a particular member. 
+    ''' <para>This is basically done by setting <C>BORRROWEDBY</C> and <c>BORROWEDON</c> to null and setting <c>AVAILABLE</c> to 1 in the <c>Book</c> table.</para>
+    ''' </summary>
+    ''' <param name="bookId">Unique ID of the book to be returned.</param>
+    ''' <returns>Number of rows affected i.e, 1 as true.</returns>
     Friend Shared Async Function Returned(bookId As String) As Task(Of Integer)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -85,7 +106,13 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' Add a book to the borrowed list.
+        ''' <para>This is basically done by setting <C>BORRROWEDBY</C> to the Unique ID of the member and <c>BORROWEDON</c> to todays date. and setting <c>AVAILABLE</c> to 0 in the <c>Book</c> table.</para>
+    ''' </summary>
+    ''' <param name="bookId">Unique ID of the book which is borrowed.</param>
+    ''' <param name="memberId">Unique ID of the member who borrows the book.</param>
+    ''' <returns></returns>
     Friend Shared Async Function Borrowed(bookId As String, memberId As String) as Task(Of Integer)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -106,7 +133,12 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' This function returns the details of the books from the <c>Book</c> table and used to update the view of the Book.
+    ''' </summary>
+    ''' <param name="bookId">Unique ID of the book.</param>
+    ''' <returns>A dynamic object which contains the details about the book.</returns>
+    ''' <seealso cref="Carrel.ViewBook"/>
     Public Shared Function GetBookById(bookId As String) As Object
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -137,7 +169,11 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' This function is used to get the number of books available with the same ISBN number.
+    ''' </summary>
+    ''' <param name="isbn">ISBN number of any book.</param>
+    ''' <returns>Returns the number of books of same ISBN</returns>
     Public Shared Function GetNumber(isbn As String) As String
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -154,6 +190,10 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
+    ''' <summary>
+    ''' Gets the total number of books available in the <c>Book</c> table.
+    ''' </summary>
+    ''' <returns>Total number of books in <c>Book</c> table.</returns>
     Public Async Shared Function TotalBooks() As Task(Of String)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -168,6 +208,11 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
+    ''' <summary>
+    ''' This function returns the total number of books issued.
+    ''' <para>Gets the count form <c>Book</c> table where <c>BORROWEDBY</c> is not <c>NULL</c></para>
+    ''' </summary>
+    ''' <returns>Total number of books issued.</returns>
     Public Async Shared Function TotalBooksIssued() As Task(Of String)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -182,6 +227,12 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
+    ''' <summary>
+    ''' Gets the number of books available in the library of a particular ISBN number.
+    ''' <para>Used in <see cref="ViewBook"/></para>
+    ''' </summary>
+    ''' <param name="isbn">ISBN number of the book.</param>
+    ''' <returns>The total number of books available of a particular ISBN</returns>
     Public Shared Function GetAvailable(isbn As String) As String
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -199,7 +250,12 @@ Public Class BookService
             query.Connection.Close()
         End Try
     End Function
-
+    ''' <summary>
+    ''' This function is used to edit books with a particualr ISBN.
+    ''' <para>The <see cref="BookForm"/> is autofilled with all the details and then the fields are converted to a dynamic object for editing the book in the database.</para>
+    ''' </summary>
+    ''' <param name="book">Dyanmic object of book</param>
+    ''' <returns>The numbers of rows affected. A possitive integer would say books are edited.</returns>
     Public async Shared Function EditBooks(book As Object) As Task(Of Integer)
         Dim connection =
                 New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
@@ -220,7 +276,11 @@ Public Class BookService
            query.Connection.Close
         End Try
     End Function
-
+    ''' <summary>
+    ''' Delete any book with an unique ID
+    ''' </summary>
+    ''' <param name="uid">Unique ID of the book.</param>
+    ''' <returns>The number of rows affected.</returns>
     Public Async Shared Function Delete(uid As String) As Task(Of Integer)
         Dim connection = New SqlConnection(Configuration.ConfigurationManager.ConnectionStrings("Carrel").ConnectionString)
         Dim query = new SqlCommand("DELETE FROM Book WHERE UID = @UID",connection)
