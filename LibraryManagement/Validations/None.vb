f98bc7261c7c4444582688e92cb6e004e5c3b074
Pratik Jain <pratik862pj@live.com>
2017-11-05 17:50:56 +0530

    :arrow_upper_right: Documnetation comments

--- LibraryManagement/Validations/None.vb
+++ LibraryManagement/Validations/None.vb
@@ -5,10 +5,10 @@ Namespace Validation
 Public Class None
     Inherits ValidationRule
         ''' <summary>
-        ''' This function is used to check if the textfield is empty
+        ''' This function is a dummy validation - returns true always.
         ''' </summary>
         ''' <param name="value">Value in the textfield.</param>
-        ''' <returns> As this function is called when the content of textfield is  changed; it would return true as the field is not empty.</returns>
+        ''' <returns> True.</returns>
     Public Overrides Function Validate(value As Object, cultureInfo As CultureInfo) As ValidationResult
            Return New ValidationResult(True,"")
     End Function
