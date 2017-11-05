f73fa0f975b52f847c5226a4eb6e3fdbfd30ec33
Pratik Jain <pratik862pj@live.com>
2017-11-05 17:52:37 +0530

    :arrow_upper_right: Documentation comments

--- LibraryManagement/Validations/NotEmpty.vb
+++ LibraryManagement/Validations/NotEmpty.vb
@@ -2,6 +2,11 @@
 Namespace Validation
 	Public Class NotEmpty
 		Inherits ValidationRule
+		''' <summary>
+		''' This function is used to check if the textfield is empty.
+		''' </summary>
+		''' <param name="value">Text present in textfield</param>
+		''' <returns>False with a message if value is null or whitespace..</returns>
 		Public Overrides Function Validate(value As Object, cultureInfo As CultureInfo) As ValidationResult
 			Return If(String.IsNullOrWhiteSpace((If(value, "")).ToString()), New ValidationResult(False, "Field is required."), ValidationResult.ValidResult)
 		End Function
