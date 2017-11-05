0ec3269991448842f9a8361a2b40b0bf259b39fc
Pratik Jain <pratik862pj@live.com>
2017-11-05 21:00:04 +0530

    ↗️ Documentation Comments

--- LibraryManagement/BookForm.xaml.vb
+++ LibraryManagement/BookForm.xaml.vb
@@ -3,10 +3,8 @@ Imports System.Text.RegularExpressions
 Imports Newtonsoft.Json
 Public Class BookForm
     Public Sub New
-
         ' This call is required by the designer.
         InitializeComponent()
-
         If DesignerProperties.GetIsInDesignMode(New DependencyObject()) Then
             Return
         Else
@@ -15,7 +13,9 @@ Public Class BookForm
         End If
 
     End Sub
-
+    ''' <summary>
+    ''' This sub procedure adds the selected Author in the combobox to Author List when <c>Tab</c> is pressed, and clears the combobox list later.
+    ''' </summary>
     Private Sub CmbAuthor_KeyDown(sender As Object, e As Input.KeyEventArgs) Handles CmbAuthor.KeyDown
         If e.Key = Key.Tab Then
             If Trim(CmbAuthor.Text) <> "" Then
@@ -25,7 +25,11 @@ Public Class BookForm
             End If
         End If
     End Sub
-
+    ''' <summary>
+    ''' This sub procedure adds the selected Author in the combobox to Author List when an item is selected and DropDown is closed, alse clears the combobox list later.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
     Private Sub CmbAuthor_DropDownClosed(sender As Object, e As EventArgs) Handles CmbAuthor.DropDownClosed
         If Trim(CmbAuthor.Text) <> "" Then
             LstAuthor.Items.Add(CmbAuthor.Text)
@@ -38,6 +42,10 @@ Public Class BookForm
         AddBook()
         AddAuthor()
     End Sub
+    ''' <summary>
+    ''' Converts the author list into JSON and creates a dynamic object of book, which is then passed to <see cref="BookService.AddBook(Object, Integer)"/> if the hidden label is empty else calls <see cref="BookService.EditBooks(Object)"/>
+    ''' </summary>
+    ''' <seealso cref="BookService"/>
     Private Async Sub AddBook()
         Dim book As Object = New Linq.JObject
         Book.ISBN = TxtISBN.Text
@@ -51,25 +59,28 @@ Public Class BookForm
         If lblUid.Content = ""
             If Await BookService.AddBook(Book, number) Then
                 DashBoard.SnackBarMessageQueue.Enqueue(TxtNumber.Text + " Book(s) Added", "PRINT QR", Sub()
-                    'Dim bookView As New ViewBook
-                    'BookView.ViewLastRecord(number)
                      MsgBox("This is an upcoming feature")
                 End Sub)
             Else
                 DashBoard.SnackBarMessageQueue.Enqueue("Failed registering ")
             End If
         Else
+            ' TODO  Work on EditBooks
             If Await BookService.EditBooks(Book) Then
                 DashBoard.SnackBarMessageQueue.Enqueue(TxtNumber.Text + " Book(s) Added", "VIEW", Sub()
                     Dim bookView As New ViewBook
                     BookView.ViewLastRecord(number)
                 End Sub)
             Else
-                DashBoard.SnackBarMessageQueue.Enqueue("Failed registering ")
+                DashBoard.SnackBarMessageQueue.Enqueue("Failed updating ")
             End If 
         End If
       
     End Sub
+    ''' <summary>
+    ''' This sub procedure is used to add Author to the database from list - if not already present in the combobox dropdown. Calls <see cref="AuthorService.AddAuthor(String)"/> to add author.
+    ''' </summary>
+    ''' <seealso cref="AuthorService"/>
     Private Sub AddAuthor()
         Dim i = 0
         For Each item In LstAuthor.Items
@@ -91,35 +102,32 @@ Public Class BookForm
         txt.GetBindingExpression(TextBox.TextProperty).UpdateSource()
     End Sub
 #End Region
-#Region "InputConstraint"
-    Private Sub TxtISBN_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtISBN.PreviewTextInput
+#Region "Input Constrations"
+    Private Sub Numeric_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) _
+        Handles TxtISBN.PreviewTextInput,
+                TxtPrice.PreviewTextInput,
+                TxtNumber.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^0-9]")
     End Sub
 
-    Private Sub TxtPublisher_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtPublisher.PreviewTextInput
-        e.Handled = Regex.IsMatch(e.Text, "[^a-zA-Z]+$")
-    End Sub
-
-    Private Sub TxtPrice_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtPrice.PreviewTextInput
-        e.Handled = Regex.IsMatch(e.Text, "[^0-9]")
-    End Sub
-
-    Private Sub TxtNumber_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtNumber.PreviewTextInput
-        e.Handled = Regex.IsMatch(e.Text, "[^0-9]")
-    End Sub
-
-    Private Sub TxtRack_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtRack.PreviewTextInput
+    Private Sub AlphaNumberic_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) _
+        Handles TxtRack.PreviewTextInput,
+                TxtTitle.PreviewTextInput,
+                TxtPublisher.PreviewTextInput
         e.Handled = Regex.IsMatch(e.Text, "[^a-zA-Z0-9]+$")
     End Sub
 
-    Private Sub TxtTitle_PreviewTextInput(sender As Object, e As TextCompositionEventArgs) Handles TxtTitle.PreviewTextInput
-        e.Handled = Regex.IsMatch(e.Text, "[^a-zA-Z0-9]+$")
-    End Sub
+
 #End Region
+    ''' <summary>
+    ''' Removes the author from selected list when the cross button (X) is pressed.
+    ''' </summary>
     Private Sub RemoveAuthorFromList(sender As Object, e As RoutedEventArgs)
         LstAuthor.Items.Remove(sender.Content)
     End Sub
-
+''' <summary>
+''' Clears all the fields in BookForm.
+''' </summary>
     Public Sub ClearAll()
         LblUID.Content = ""
         TxtPublisher.Clear()
